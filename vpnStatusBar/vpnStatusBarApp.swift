ca3ce85d6f6db34f4173ea84b092744d3b3c2684
fabiojaniolima <contato@fabiojanio.com>
2024-03-23 22:19:14 -0300

    Initial commit

---
+++ vpnStatusBar/vpnStatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  vpnStatusBarApp.swift
+//  vpnStatusBar
+//
+//  Created by Fabio J L Ferreira on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct vpnStatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
